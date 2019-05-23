09625b0b162225f46d238e2fa6c11a57e2107d02
Jonas Moss <jonas.gjertsen@gmail.com>
2019-05-23 19:25:03 +0200

    Fix of Cuddy data.

--- R/data.R
+++ R/data.R
@@ -1,20 +1,29 @@
-#' Arabidopsis QTL data on gravitropism
+#' Studies on the Effect of Power Posing
 #'
-#' Data from a QTL experiment on gravitropism in
-#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
-#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
-#' increments over eight hours.
+#' Results from 27 studies related to power posing.
 #'
-#' @usage data(dat.cuddy2018)
+#' The data points are taken from the p-curve analysis of Cuddy et al. (2018),
+#'    restricted to 2 cell designs with mean difference as the outcome
+#'    variable.
 #'
-#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#' @usage dat.cuddy2018
+#'
+#' @format The data frame contains the following columns:
+#'     \tabular{lll}{
+#'         \strong{author}       \tab \code{character} \tab first author \cr
+#'         \strong{year}         \tab \code{numeric}   \tab publication year \cr
+#'         \strong{power}        \tab \code{boolean}   \tab if \code{TRUE}, the outcome was feelin of power \cr
+#'         \strong{ease}         \tab \code{boolean}   \tab if \code{TRUE}, the outcome was an EASE variable \cr
+#'         \strong{yi}           \tab \code{numeric}   \tab standardized mean difference \cr
+#'         \strong{vi}           \tab \code{numeric}   \tab corresponding sampling variance
+#'    }
 #'
 #' @keywords datasets
 #'
-#' @references Moore et al. (2013) Genetics 195:1077-1086
-#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#' @references
+#'     Cuddy, A. J., Schultz, S. J., & Fosse, N. E. (2018). P-curving a more comprehensive body of research on postural feedback reveals clear evidential value for power-posing effects: Reply to Simmons and Simonsohn (2017). Psychological science, 29(4), 656-666.
 #'
-#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
+#' @source \url{https://osf.io/jx3av/}
 #'
 #' @examples
 #' data(grav)
@@ -23,23 +32,33 @@
 #' \donttest{iplotCurves(phe, times)}
 "dat.cuddy2018"
 
-#' Anderson 2010
+#' Studies on Effect of Violent Video Games on Negative Outcomes
 #'
-#' Data from a QTL experiment on gravitropism in
-#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
-#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
-#' increments over eight hours.
+#' Results from 477 studies on the effect of violent video games on negative
+#'    outcomes.
 #'
-#' @usage data(dat.anderson2010)
+#' @usage dat.anderson2010
 #'
-#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
-#'
+#' @format The data frame contains the following columns:
+#'     \tabular{lll}{
+#'         \strong{author}       \tab \code{character} \tab first author \cr
+#'         \strong{year}         \tab \code{numeric}   \tab publication year \cr
+#'         \strong{outcome}      \tab \code{character} \tab one of seven outcomes \cr
+#'         \strong{best   }      \tab \code{boolean}   \tab if \code{TRUE}, the was a best practice study \cr
+#'         \strong{experimental} \tab \code{boolean}   \tab if \code{TRUE}, the study was experimental \cr
+#'         \strong{adult}        \tab \code{boolean}   \tab if \code{TRUE}, the study subjects were adults \cr
+#'         \strong{country}      \tab \code{character} \tab country of study \cr
+#'         \strong{ni}           \tab \code{numeric}   \tab sample size \cr
+#'         \strong{yi}           \tab \code{numeric}   \tab observed mean difference in outcome (violent vs. non-violent) \cr
+#'         \strong{vi}           \tab \code{numeric}   \tab corresponding sampling variance
+#'    }
 #' @keywords datasets
 #'
-#' @references Moore et al. (2013) Genetics 195:1077-1086
-#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#' @references
+#'     Baskerville, N. B., Liddy, C., & Hogg, W. (2012). Systematic review and meta-analysis of practice facilitation within primary care settings. The Annals of Family Medicine, 10(1), 63-74.
+#'     Hilgard, J., Engelhardt, C. R., & Rouder, J. N. (2017). Overstated evidence for short-term effects of violent games on affect and behavior: A reanalysis of Anderson et al.(2010).
 #'
-#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
+#' @source \url{https://github.com/Joe-Hilgard/Anderson-meta}
 #'
 #' @examples
 #' data(grav)
--- data_raw/dat.cuddy2018.R
+++ data_raw/dat.cuddy2018.R
@@ -18,36 +18,98 @@ library("magrittr")
 
 dat.cuddy2018 = readxl::read_xlsx("data_raw/dat.cuddy2018.xlsx")
 
-dat.cuddy2018 %>%
-  dplyr::filter(!is.na(`(Main) Results`)) %>%
-  dplyr::select(study = Study, results = `(Main) Results`) ->
-  dat.cuddy2018
+# Find the subsets
+dat.cuddy2018$subset = "Non-EASE"
+dat.cuddy2018$subset[!is.na(dat.cuddy2018$`(Feelings of Power) Results`)] = "Power"
+dat.cuddy2018$subset[!is.na(dat.cuddy2018$`(EASE Subset) Results`)] = "EASE"
 
-splited = matrix(unlist(strsplit(dat.cuddy2018$results, "=")), nrow = 2)
-z = as.numeric(splited[2, ])
-z = sqrt(z)*(substr(splited[1, ], 1, 1) != "t") +
-    z*(substr(splited[1, ], 1, 1) == "t")
+# Make yis and vis.
+dat.cuddy2018$vi = 2/dat.cuddy2018$df
+dat.cuddy2018$yi = dat.cuddy2018$value
+dat.cuddy2018$yi[dat.cuddy2018$test != "t"] = sqrt(dat.cuddy2018$yi[dat.cuddy2018$test != "t"])
+dat.cuddy2018$yi = dat.cuddy2018$yi*sqrt(dat.cuddy2018$vi)
 
-df = c(83,  83,  40,  83,  26,  67,  39, Inf, Inf, Inf,
-       164, 60, Inf,  34,  95, Inf, 147, 126,  73, Inf,
-       53,  68,  79,  39,  28,  70, 101,  79,  58, 196,
-       20,  37,  37,  18,  16,  18,  51, Inf,  37, Inf,
-       79,  58,  84, Inf, 118,  87,  80,  58, Inf, Inf,
-       29,  66,  Inf)
+# Add year and metafor-compliant authors.
+dat.cuddy2018$year = c(2012, 2012,
+         2010, 2010,
+         2010, 2010,
+         2014, 2015,
+         2015, 2016,
+         2011, 2015,
+         2015, 2016,
+         2015, 2015,
+         1983, 1984,
+         1982, 1982,
+         2013, 2016,
+         2016, 2015,
+         2015, 2016,
+         2016, 2016,
+         2004, 2013,
+         2013)
 
-dat.cuddy2018 %>%
-  dplyr::filter(!is.infinite(df)) ->
-  dat.cuddy2018
+dat.cuddy2018$author = c("Arnette et al.", "Bohns et al.",
+           "Carney et al.", "Carney et al.",
+           "Carney et al.", "Carney et al.",
+           "Ceunen", "Cuddy et al.",
+           "Cuddy et al.", "Duffy et al.",
+           "Fischer et al.", "Fuller et al.",
+           "Kwon et al.", "Peper et al.",
+           "Ranehill et al.", "Ranehill et al.",
+           "Riskind", "Riskind",
+           "Riskind et al.", "Riskind et al.",
+           "Rotella et al.", "Teh et al.",
+           "Teh et al.", "Turan",
+           "Turan", "Veenstra et al.",
+           "Veenstra et al.", "Wilkes et al.",
+           "Wilson et al.", "Yap et al.",
+           "Yap et al.")
 
-dat.cuddy2018$vi = 2/(df[!is.infinite(df)])
-dat.cuddy2018$yi = z[!is.infinite(df)]*sqrt(dat.cuddy2018$vi)
+# Filter out chisq
+dat.cuddy2018 = dplyr::filter(dat.cuddy2018, test != "chi2")
+
 
 dat.cuddy2018 = dplyr::transmute(dat.cuddy2018,
-                                 author = study,
+                                 author = author ,
+                                 year = year,
+                                 subset = subset,
                                  yi = yi,
                                  vi = vi)
 
+# Remove outlier
+dat.cuddy2018 = dplyr::filter(dat.cuddy2018, yi < 1.5)
+
 save(dat.cuddy2018, file = "data/dat.cuddy2018.rda")
-rm(splited, df, z)
 load("data/dat.cuddy2018.rda")
 rm(dat.cuddy2018)
+
+#
+# dat.cuddy2018 %>%
+#   dplyr::filter(!is.na(`(Main) Results`)) %>%
+#   dplyr::select(study = Study, results = `(Main) Results`) ->
+#   dat.cuddy2018
+#
+# splited = matrix(unlist(strsplit(dat.cuddy2018$results, "=")), nrow = 2)
+# z = as.numeric(splited[2, ])
+# z = sqrt(z)*(substr(splited[1, ], 1, 1) != "t") +
+#     z*(substr(splited[1, ], 1, 1) == "t")
+#
+# df = c(83,  83,  40,  83,  26,  67,  39, Inf, Inf, Inf,
+#        164, 60, Inf,  34,  95, Inf, 147, 126,  73, Inf,
+#        53,  68,  79,  39,  28,  70, 101,  79,  58, 196,
+#        20,  37,  37,  18,  16,  18,  51, Inf,  37, Inf,
+#        79,  58,  84, Inf, 118,  87,  80,  58, Inf, Inf,
+#        29,  66,  Inf)
+#
+# dat.cuddy2018 %>%
+#   dplyr::filter(!is.infinite(df)) ->
+#   dat.cuddy2018
+#
+# dat.cuddy2018$vi = 2/(df[!is.infinite(df)])
+# dat.cuddy2018$yi = z[!is.infinite(df)]*sqrt(dat.cuddy2018$vi)
+#
+# dat.cuddy2018 = dplyr::transmute(dat.cuddy2018,
+#                                  author = study,
+#                                  yi = yi,
+#                                  vi = vi)
+# rm(splited, df, z)
+
