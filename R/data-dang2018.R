e0b76f55968c7553bf47ca7c86dfc116c09c4f07
Jonas Moss <jonas.gjertsen@gmail.com>
2019-11-07 11:03:49 +0100

    :recycle: Refactored data R files.

---
+++ R/data-dang2018.R
@@ -0,0 +1,17 @@
+#' Dang 2018
+#'
+#' Results from 150 studies of ego depletion, the claim that self-control is a
+#'     limited resource which is tapped whenever self-control is exerted.
+#'
+#' @usage data(dat.dang2018)
+#'
+#' @format The data frame contains the following columns:
+#'
+#'
+#' @keywords datasets
+#'
+#' @references Moore et al. (2013) Genetics 195:1077-1086
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
+"dat.dang2018"
